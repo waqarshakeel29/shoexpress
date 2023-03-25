7cdc1c6c26cb0ed235ba007179bc9bdf432e8601
Waqar Shakeel <waqarshakeel29@gmail.com>
2023-03-26 00:57:03 +0500

    animation removed from popular now section

--- src/pages/home-page/popular-articles-section/index.tsx
+++ src/pages/home-page/popular-articles-section/index.tsx
@@ -2,23 +2,8 @@ import { Button, Grid, Typography } from "@mui/material";
 import article1 from "../../../icons/article1.png";
 import article1Hover from "../../../icons/article1-hover.png";
 import { ArticleItem } from "./article-item.tsx";
-import { motion } from "framer-motion";
 
 export const PopularArticlesSection = () => {
-  const imageVariants = {
-    offscreen: {
-      x: 200
-    },
-    onscreen: {
-      x: 0,
-      transition: {
-        type: "spring",
-        bounce: 0.4,
-        duration: 0.8,
-      },
-    },
-  };
-
   const imageList = [
     {
       label: "article1",
@@ -84,85 +69,78 @@ export const PopularArticlesSection = () => {
 
   return (
     <>
-      <motion.div
-        variants={imageVariants}
-        initial="offscreen"
-        whileInView="onscreen"
-        viewport={{ once: true, amount: 0.7 }}
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        sx={{ minHeight: "100vh", pb: 3, px: 5 }}
       >
-        <Grid
-          container
-          justifyContent="center"
-          alignItems="center"
-          sx={{ minHeight: "100vh", pb: 3, px: 5 }}
-        >
-          <Grid item xs={12}>
-            <Typography
-              sx={{
-                textAlign: "center",
-                fontFamily: "Stardos Stencil",
-                fontStyle: "normal",
-                fontWeight: 400,
-                fontSize: "48px",
-                pt: 10,
-              }}
-            >
-              POPULAR RIGHT NOW
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Grid
-              container
-              spacing={2}
-              justifyContent="center"
-              alignItems="center"
-            >
-              {["Sneakers", "Sport Shoes", "Oxford", "Sale"].map((article) => (
-                <Grid item>
-                  <Button
-                    color="primary"
-                    variant="outlined"
-                    onClick={() => {}}
-                    sx={{ borderRadius: "20px" }}
-                  >
-                    {article}
-                  </Button>
-                </Grid>
-              ))}
-            </Grid>
-          </Grid>
-          <Grid item xs={12} sx={{ textAlign: "left" }}>
-            <Typography
-              sx={{
-                fontFamily: "Didact Gothic",
-                fontStyle: "normal",
-                fontWeight: 700,
-                fontSize: "36px",
-              }}
-            >
-              New Arrival
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Grid
-              container
-              sx={{
-                overflowX: "scroll",
-                flexWrap: "nowrap",
-              }}
-              spacing={3}
-            >
-              {imageList.map((article) => (
-                <ArticleItem
-                  article={article.label}
-                  image={article.image}
-                  hoveredImage={article.hoveredImage}
-                />
-              ))}
-            </Grid>
+        <Grid item xs={12}>
+          <Typography
+            sx={{
+              textAlign: "center",
+              fontFamily: "Stardos Stencil",
+              fontStyle: "normal",
+              fontWeight: 400,
+              fontSize: "48px",
+              pt: 10,
+            }}
+          >
+            POPULAR RIGHT NOW
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid
+            container
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+          >
+            {["Sneakers", "Sport Shoes", "Oxford", "Sale"].map((article) => (
+              <Grid item>
+                <Button
+                  color="primary"
+                  variant="outlined"
+                  onClick={() => {}}
+                  sx={{ borderRadius: "20px" }}
+                >
+                  {article}
+                </Button>
+              </Grid>
+            ))}
           </Grid>
         </Grid>
-      </motion.div>
+        <Grid item xs={12} sx={{ textAlign: "left" }}>
+          <Typography
+            sx={{
+              fontFamily: "Didact Gothic",
+              fontStyle: "normal",
+              fontWeight: 700,
+              fontSize: "36px",
+            }}
+          >
+            New Arrival
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid
+            container
+            sx={{
+              overflowX: "scroll",
+              flexWrap: "nowrap",
+            }}
+            spacing={3}
+          >
+            {imageList.map((article) => (
+              <ArticleItem
+                article={article.label}
+                image={article.image}
+                hoveredImage={article.hoveredImage}
+              />
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 };
